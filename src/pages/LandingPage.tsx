389bea6c25b3f338553c07bdb103948dfafe883f
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2022-06-24 21:34:37 -0300

    minor adjust

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -37,7 +37,7 @@ export function LandingPage() {
   }
 
   return (
-    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center max-h-full">
       <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
         <div className="max-w-[640px]">
           <Logo />
